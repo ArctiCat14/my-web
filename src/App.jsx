47e415aceeb49ca7b5401eb5fd5c0126860ef7f2
ArctiCat14 <1834117863@qq.com>
2026-01-20 12:54:13 +0800

    web page

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,14 @@ import { useState, useEffect } from "react";
 import { Typography } from "antd";
 import { ConfigProvider } from "antd";
 import { theme } from "antd";
-import { Space, Button, Switch, FloatButton } from "antd";
+import { Layout } from "antd";
+import { Space, Button, Switch, FloatButton, Flex } from "antd";
+import { Col, Row } from "antd";
 import { BulbOutlined } from "@ant-design/icons";
 import "antd/dist/reset.css";
 
 const { Title, Text } = Typography;
+const { Header, Content, Footer } = Layout;
 
 function App() {
     const [isDarkMode, setIsDarkMode] = useState(
@@ -20,33 +23,100 @@ function App() {
         return () => mediaQuery.removeEventListener("change", handleChange);
     }, []);
 
+    const {
+        token: { borderRadiusLG },
+    } = theme.useToken();
+
     return (
-        <ConfigProvider
-            theme={{
-                algorithm: isDarkMode
-                    ? theme.darkAlgorithm
-                    : theme.defaultAlgorithm,
-            }}
-        >
-            <div
-                style={{
-                    padding: "20px",
-                    maxWidth: "device-width",
-                    margin: "0 auto",
-                    minHeight: "100vh",
-                    backgroundColor: isDarkMode ? "#303030" : "#dddddd",
-                    color: isDarkMode ? "#dddddd" : "#303030",
-                    transition: "background-color 0.35s ease",
+        <Layout>
+            <ConfigProvider
+                theme={{
+                    algorithm: theme.darkAlgorithm,
                 }}
             >
-                <Space align="center" style={{ marginBottom: "20px" }}>
-                    <Button
-                        icon={<BulbOutlined />}
-                        onClick={() => setIsDarkMode(!isDarkMode)}
-                    />
-                </Space>
-            </div>
-        </ConfigProvider>
+                <Header
+                    style={{
+                        backgroundColor: isDarkMode ? "#000000" : "#202020",
+                        transition: "background-color 0.3s ease",
+                    }}
+                >
+                    <Row>
+                        <Col flex={31}>
+                            <div
+                                style={{
+                                    fontSize: "24px",
+                                    fontWeight: "bold",
+                                    color: "#dddddd",
+                                }}
+                            >
+                                Nothing's Website
+                            </div>
+                        </Col>
+                        <Col flex={1}>
+                            <Button
+                                icon={<BulbOutlined />}
+                                onClick={() => setIsDarkMode(!isDarkMode)}
+                            />
+                        </Col>
+                    </Row>
+                </Header>
+            </ConfigProvider>
+            <ConfigProvider
+                theme={{
+                    algorithm: isDarkMode
+                        ? theme.darkAlgorithm
+                        : theme.defaultAlgorithm,
+                }}
+            >
+                <Content
+                    style={{
+                        padding: "20px",
+                        backgroundColor: isDarkMode ? "#303030" : "#ffffff",
+                        transition: "background-color 0.3s ease",
+                    }}
+                >
+                    <Flex
+                        style={{
+                            textAlign: "center",
+                            padding: " 20px 120px 480px 20px",
+                        }}
+                        gap="middle"
+                        align="start"
+                        vertical
+                    >
+                        <Title
+                            style={{
+                                margin: "0px",
+                                fontSize: "64px",
+                                fontWeight: "bold",
+                            }}
+                        >
+                            What can i say.
+                        </Title>
+                        <Title
+                            style={{
+                                marginTop: "0px",
+                            }}
+                            level={2}
+                            type="secondary"
+                        >
+                            mamba out
+                        </Title>
+                        <Flex gap="small" wrap>
+                            <Button type="primary">Man</Button>
+                            <Button>What can i say</Button>
+                        </Flex>
+                    </Flex>
+                </Content>
+            </ConfigProvider>
+            <Footer
+                style={{
+                    textAlign: "center",
+                    backgroundColor: isDarkMode ? "#000000" : "#202020",
+                    transition: "background-color 0.3s ease",
+                }}
+            ></Footer>
+        </Layout>
     );
 }
 
