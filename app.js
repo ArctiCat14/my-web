c66c3e4319fe5790232d6777273b10f4a8f25536
ArctiCat14 <1834117863@qq.com>
2026-02-10 18:17:11 +0800

    file server

--- app.js
+++ app.js
@@ -2,12 +2,16 @@ import express from "express";
 import path from "path";
 
 const app = express();
-const __dirname = path.resolve();
+const CONTAINER_ROOT = "/app";
+const DIST_DIR = path.join(CONTAINER_ROOT, "dist");
+const RESOURCE_DIR = path.join(CONTAINER_ROOT, "resource");
 
-app.use(express.static(path.join(__dirname, "dist")));
+app.use("/resource", express.static(RESOURCE_DIR));
+
+app.use(express.static(DIST_DIR));
 
 app.use((req, res) => {
-    res.sendFile(path.join(__dirname, "dist", "index.html"));
+    res.sendFile(path.join(DIST_DIR, "index.html"));
 });
 
 app.listen(80, "0.0.0.0", () => {});
