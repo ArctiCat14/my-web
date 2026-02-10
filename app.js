1c4f7166b8f40f2476d619e5b57a44566dfb8b43
ArctiCat14 <1834117863@qq.com>
2026-02-10 22:39:18 +0800

    cdn

--- app.js
+++ app.js
@@ -1,17 +1,26 @@
 import express from "express";
 import path from "path";
+import config from "./config/config.js";
 
 const app = express();
 const CONTAINER_ROOT = "/app";
 const DIST_DIR = path.join(CONTAINER_ROOT, "dist");
 const RESOURCE_DIR = path.join(CONTAINER_ROOT, "resource");
 
-app.use("/resource", express.static(RESOURCE_DIR));
-
 app.use(express.static(DIST_DIR));
-
 app.use((req, res) => {
     res.sendFile(path.join(DIST_DIR, "index.html"));
+});
+
+app.use("/resource", express.static(RESOURCE_DIR));
+app.use("/resource", (req, res) => {
+    const cdnDomain = config.cdnDomain;
+    const filePath = req.path;
+    if (!cdnDomain) {
+        return res.status(400);
+    }
+
+    res.redirect(`${cdnDomain}${filePath}`);
 });
 
 app.listen(80, "0.0.0.0", () => {});
---
+++ config/config.js
@@ -0,0 +1,3 @@
+export default {
+    cdnDomain: "https://modpack-1328443549.cos.ap-shanghai.myqcloud.com",
+};
