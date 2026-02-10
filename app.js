7acab3c6bb8d83d6f42865ace5c12ff966ec99c0
ArctiCat14 <1834117863@qq.com>
2026-02-10 22:53:23 +0800

    cdn

--- app.js
+++ app.js
@@ -5,14 +5,7 @@ import config from "./config/config.js";
 const app = express();
 const CONTAINER_ROOT = "/app";
 const DIST_DIR = path.join(CONTAINER_ROOT, "dist");
-const RESOURCE_DIR = path.join(CONTAINER_ROOT, "resource");
 
-app.use(express.static(DIST_DIR));
-app.use((req, res) => {
-    res.sendFile(path.join(DIST_DIR, "index.html"));
-});
-
-app.use("/resource", express.static(RESOURCE_DIR));
 app.use("/resource", (req, res) => {
     const cdnDomain = config.cdnDomain;
     const filePath = req.path;
@@ -21,6 +14,11 @@ app.use("/resource", (req, res) => {
     }
 
     res.redirect(`${cdnDomain}${filePath}`);
+});
+
+app.use(express.static(DIST_DIR));
+app.use((req, res) => {
+    res.sendFile(path.join(DIST_DIR, "index.html"));
 });
 
 app.listen(80, "0.0.0.0", () => {});
